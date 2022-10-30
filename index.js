85b60a0f962199b6eb46940b9d2716293756cdb5
WoodComet <goneboy0623@gmail.com>
2022-10-30 12:28:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ server.on('message',function(msg,info){
     console.log('Data received from client : ' + msg.toString());
     console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
 
-    if(!getUserByInfo(info) != undefined){
+    if(getUserByInfo(info) == undefined){
 
         if(msg.substring(0, 10) == 'CMD:Login:'){
             
             createUser(info, msg.substring(6));
-            return;
+            
         }
+        return;
     }
     //handel client commands
     if(msg == "HB"){
