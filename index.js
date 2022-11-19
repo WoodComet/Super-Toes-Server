57bf7d900cb5651db631336bf6a494c81133b46c
WoodComet <goneboy0623@gmail.com>
2022-11-18 21:49:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ function getRoomByName(name){
     for(i in roomlist){
         var Room = roomlist[i];
         console.log(Room.name + "is a room" + name + "?");
-        if(Room.name === name){
+        if(Room.name == name){
             console.log("found room: " + Room.name);
             return Room;
         }
